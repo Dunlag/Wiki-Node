105313ae436fe14f6ee87700d2d462335c99aa6a
practicasNetsolutions <practicas@netsolutions.es>
2025-01-23 13:56:18 +0100

    creacion y uso de modulos acabado y listo

--- Introduccion a NodeJS/02 - Creacion y uso de modulos/index.js
+++ Introduccion a NodeJS/02 - Creacion y uso de modulos/index.js
@@ -1,1 +1,15 @@
+const operaciones = require('./operaciones');
+const Persona = require('./persona');
+
+const suma = operaciones.sumar(3,8);
+const restar = operaciones.restar(3,8);
+const multiplicar = operaciones.mult(3,8);
+
+console.log(suma);
+console.log(restar);
+console.log(multiplicar);
+
+const pers = new Persona('Elena', 'Gomez', 21);
+
+console.log(pers.mostrar());
 
