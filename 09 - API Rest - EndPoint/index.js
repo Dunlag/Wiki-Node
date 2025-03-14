58ee854ea55ac482db87bea7abd5c01739ae25eb
practicasNetsolutions <practicas@netsolutions.es>
2025-03-14 13:05:22 +0100

    protegiendo contraseña

--- 09 - API Rest - EndPoint/index.js
+++ 09 - API Rest - EndPoint/index.js
@@ -1,23 +1,21 @@
+require("dotenv").config();
 const mongoose = require("mongoose");
 const app = require("./app");
 const port = 3000;
 
-//const urlMongoDB = "mongodb+srv://admin:XkTI39KmYJFLEkTg@fernandodb.sdyqz.mongodb.net/";
+const urlMongoDB = process.env.MONGODB_URI; // Se toma la variable del .env
 
 const conectarDB = async () => {
     try {
         await mongoose.connect(urlMongoDB);
-
         console.log("✅ Conectado a MongoDB correctamente");
 
         app.listen(port, () => {
             console.log(`🚀 Servidor del API REST funcionando en http://localhost:${port}`);
         });
-
     } catch (error) {
         console.error("❌ Error al conectar a la base de datos:", error);
     }
 };
 
-// Llamamos a la función para conectar a la BD
 conectarDB();
